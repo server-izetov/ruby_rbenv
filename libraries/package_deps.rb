f7270f0b102d9177510864490a2feb0076fe5380
Server Izetov <izetov.server@gmail.com>
2021-01-17 17:31:16 +0300

    temporaly fix for debian 10

--- libraries/package_deps.rb
+++ libraries/package_deps.rb
@@ -45,10 +45,8 @@ class Chef
             %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev make)
           elsif platform?('ubuntu') && node['platform_version'].to_i >= 18
             %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev make)
-          elsif platform?('debian') && node['platform_version'].to_i >= 10
-            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev make)
           else
-            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev make)
+            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev make)
           end
         when 'suse'
           %w(gcc make automake gdbm-devel libyaml-devel ncurses-devel readline-devel zlib-devel libopenssl-devel bzip2)
